d23b9af906e491b50bd3634c4bcacc5de2d36803
Choudhary8055 <80520509+Choudhary8055@users.noreply.github.com>
2022-05-23 09:23:48 +0530

    aaa

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <div data-ns-test="project-name">Netflix-Clone</div>
+      <div data-ns-test="project-name">Form-Validation</div>
+    </div>
+  );
+};
 
 export default App;
